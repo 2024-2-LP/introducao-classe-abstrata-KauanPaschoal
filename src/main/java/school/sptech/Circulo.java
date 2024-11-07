a58da2da321e3749d51d9a479ac1a635f373be60
KauanPaschoal <kauan.psantos@sptech.school>
2024-11-07 18:10:39 -0300

    Exercicio finalizado

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,23 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura{
+
+    private Double raio;
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
+    @Override public Double calcularArea(){
+        Double area = Math.PI * (raio * raio);
+        return area;
+    }
+
+    @Override public String toString(){
+        return "O circulo tem area de " + calcularArea();
+    }
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,56 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras = new ArrayList<>();
+
+    public List<Figura> getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List<Figura> figuras) {
+        this.figuras = figuras;
+    }
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double soma = 0.0;
+        Double area = 0.0;
+        for (Figura figura : figuras) {
+            area = figura.calcularArea();
+            soma += area;
+        }
+        return soma;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        Double area = 0.0;
+        List<Figura> figuraMaior = new ArrayList<>();
+        for (Figura figura : figuras) {
+            area = figura.calcularArea();
+            if (area > 20){
+                figuraMaior.add(figura);
+            }
+
+        }
+        return figuraMaior;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> FigurasQuadrado = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura instanceof Quadrado){
+                FigurasQuadrado.add(figura);
+            }
+        }
+        return FigurasQuadrado;
+
+
+    }
 }
